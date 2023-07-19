3cd0f6e2da50be45ff027464615b9b006c930d6a
banqingyuan <15606160125@163.com>
2023-07-19 17:42:51 +0800

    presist ai and change tpl loading

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='unichat-prompt-sdk',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     package_data={"unichat-prompt-sdk": ["prompt_factory/tpl/*.yml"]},
-    include_package_data=True,
+    include_package_data=True,  # 这是关键：确保包含MANIFEST.in中指定的所有文件
     install_requires=[
         'pydantic==1.10.9',
         'redis==4.5.5',
