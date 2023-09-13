a0286291b4eb3cf5e1c73eb82bbfc012163583e9
banqingyuan <15606160125@163.com>
2023-09-13 15:17:54 +0800

    add action sharing_params

--- action_strategy/action.py
+++ action_strategy/action.py
@@ -17,21 +17,25 @@ logger = wrapper_azure_log_handler(
 
 
 class Action(BaseModel):
-    # todo 校验action的有效性
-    # todo 有效action可枚举，登记使用方法
+    """
+    todo 校验action的有效性
+    todo 有效action可枚举，登记使用方法
+    Action 是用于执行场景触发的动作，也包括未来的蓝图动作
+    action name是枚举值，由项目代码实现不同的action执行逻辑。
+    每种action需要的参数种类是固定的，在sharing_params中提供
+    参数生成逻辑不一而足，统一在action_script中把必要参数填充进sharing_params
+    """
 
     action_name: str
     queuing_time: int = 0
     action_script: str = None
     sharing_params: Dict = {}
-    execute_result: Any = None
 
     def pre_loading(self, event: SceneEvent, **factor_value):
         if not self.action_script or self.action_script == '':
             return
         input_params = {
             'trigger_event': event,
-            'execute_result': None,
             'sharing_params': self.sharing_params,
             **factor_value,
         }
