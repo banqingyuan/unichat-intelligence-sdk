2d7c3dfd13641ee3a4a76f72fcbe480139aa8522
banqingyuan <15606160125@163.com>
2024-01-04 21:53:25 +0800

    优化聊天历史查询

--- memory_sdk/instance_memory_block/query_util.py
+++ memory_sdk/instance_memory_block/query_util.py
@@ -93,13 +93,14 @@ def _generate_query_filter_by_query_option(query_option: QueryOption):
 
 
 def _save_to_csv(file_name: str, block_lst: list):
-    AI_dict: Dict = {}
-    for block in block_lst:
-        if block.AID not in AI_dict:
-            AI_dict[block.AID] = []
-        AI_dict[block.AID].append(block)
-    for AID, block_lst in AI_dict.items():
-        AI_dict[AID] = sorted(block_lst, key=lambda x: x.create_timestamp, reverse=True)
+    # AI_dict: Dict = {}
+    # for block in block_lst:
+    #     if block.AID not in AI_dict:
+    #         AI_dict[block.AID] = []
+    #     AI_dict[block.AID].append(block)
+    # for AID, block_lst in AI_dict.items():
+    #     AI_dict[AID] = sorted(block_lst, key=lambda x: x.create_timestamp, reverse=True)
+    block_lst = sorted(block_lst, key=lambda x: x.create_timestamp)
 
     # last_time_lst = []
     # for AID, block_lst in AI_dict.items():
@@ -112,15 +113,15 @@ def _save_to_csv(file_name: str, block_lst: list):
     with open(f'{file_name}.csv', 'w', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(['AID', 'role: speaker', 'message', 'occur time'])
-        for AID, block_lst in AI_dict.items():
-            for block in block_lst:
-                for event in block.origin_event:
-                    if isinstance(event, ConversationEvent):
-                        if event.role == 'AI' and AID == '':
-                            AID = event.speaker
-                        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(event.occur_time)))
-                        writer.writerow([AID, f"{event.role}: {event.speaker_name}", event.message, formatted_time])
-                writer.writerow(['', '', ''])
+        for block in block_lst:
+            AID = ''
+            for event in block.origin_event:
+                if isinstance(event, ConversationEvent):
+                    if event.role == 'AI' and AID == '':
+                        AID = event.speaker
+                    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(event.occur_time)))
+                    writer.writerow([AID, f"{event.role}: {event.speaker_name}", event.message, formatted_time])
+            writer.writerow(['', '', ''])
 
 
 def query_conversation_history(file_name: str, query_option: QueryOption, limit: int = None):
@@ -131,12 +132,10 @@ def query_conversation_history(file_name: str, query_option: QueryOption, limit:
     res = mongodb_client.find_from_collection('AI_memory_block', filter=mongo_filter, limit=limit)
     for item in res:
         block_lst.append(from_mongo_res_to_event_block(item))
+    # sort by create_timestamp
     _save_to_csv(file_name, block_lst)
 
 
 if __name__ == '__main__':
-    query_option = QueryOption().set_time_range(start_time='2020-09-01', end_time='2024-12-30').contain('甘木').with_AIDs('22202678-f2443fa238').with_UIDs('22202679', '22202678').with_max_round_number(6)
+    query_option = QueryOption().set_time_range(start_time='2020-09-01 00:00:00', end_time='2024-12-30 23:59:59').contain('甘木').with_UIDs('22202679', '22202678')
     query_conversation_history('test', query_option, 10)
-
-
-
